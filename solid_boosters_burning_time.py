74de0b40cd4477cba659e2f9bae31dc56ccaeb8d
Ros <kukishvmasle@gmail.com>
2025-09-08 12:07:37 +0200

    Added warnings for thrust limiter coefficients exceeding 1 in solid_boosters_burning_time.py

--- solid_boosters_burning_time.py
+++ solid_boosters_burning_time.py
@@ -1,13 +1,21 @@
-def get_positive_number(prompt: str, num_type: float):
+def get_positive_number(promp: str, num_type: float):
     while True:
         try:
-            value = num_type(input(prompt))
+            value = num_type(input(promp))
             if value <= 0:
                 print("Value must be greater than zero. Please try again.")
                 continue
             return value
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
+
+def check_trust(name: str, total_fuel: int, fuel_consumption: float, burning_time: float)->None:
+    coeff = total_fuel / (burning_time * fuel_consumption)
+
+    if coeff > 1:
+        print(f"Warning: {name} thrust limiter coefficient exceeds 1. Means the burning time is too short. Additional fuel is required.")
+    else:
+        print(f"{name} thrust limiter is set to {coeff:.3f}")
 
 def main():
     big_booster_total_fuel = get_positive_number("Enter big booster total fuel: ", int)
@@ -19,8 +27,8 @@ def main():
     big_booster_thrust_limiter_coeff = big_booster_total_fuel / (boosters_burning_time * big_booster_fuel_consumption)
     small_booster_thrust_limiter_coeff = small_booster_total_fuel / (boosters_burning_time * small_booster_fuel_consumption)
 
-    print(f"Big booster thrust limiter is set to {big_booster_thrust_limiter_coeff:.3f}")
-    print(f"Small booster thrust limiter is set to {small_booster_thrust_limiter_coeff:.3f}")
+    check_trust("Big Booster", big_booster_total_fuel, big_booster_fuel_consumption, boosters_burning_time)
+    check_trust("Small Booster", small_booster_total_fuel, small_booster_fuel_consumption, boosters_burning_time)
 
 if __name__ == "__main__":
     main()
